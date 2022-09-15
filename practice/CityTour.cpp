ee0420777d00389e14a170275a97ff1c5e1f3525
supratim531 <91423964+supratim531@users.noreply.github.com>
2022-09-15 17:52:05 +0530

    supratim531: done CityTour from https://www.hackerearth.com/problem/algorithm/city-tour/

---
+++ practice/CityTour.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerearth.com/problem/algorithm/city-tour/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void run_case() {
+	uint64_t n, a;
+	cin >> n;
+	cout << n * 4LL << '\n';
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while (t --> 0)
+		run_case();
+	return 0;
+}
